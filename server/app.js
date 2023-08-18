8f7065bf430c0bfa4bb3531461b1850ba0cd1437
pratham-25 <prathu2mk@gmail.com>
2023-08-19 00:23:40 +0530

    update client url in server

--- server/app.js
+++ server/app.js
@@ -1,24 +1,27 @@
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
-const express = require('express')
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
-dotenv.config({ path: './config.env' })
-require('./db/conn')
-app.use(cors({
-    origin: '*'
-}));
-app.use(express.json());        // to convert data in form of json
+dotenv.config({ path: "./config.env" });
+require("./db/conn");
+app.use(
+  cors({
+    origin: ["https://hostel-portal-client.vercel.app"],
+    methods: "*",
+    credentials: true,
+  })
+);
+app.use(express.json()); // to convert data in form of json
 
 // const User = require('./model/userSchema')
 
 // gettings the routes
-app.use(require('./router/auth'))
+app.use(require("./router/auth"));
 
-const PORT = process.env.PORT
-
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`listening to port ${PORT}`);
-})
+  console.log(`listening to port ${PORT}`);
+});
